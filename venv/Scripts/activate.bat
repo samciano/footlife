8a881a6215a51aa44947ae04d4f841d34cdd6891
Samuel Machado <samuelmachado1503@gmail.com>
2026-08-20 15:42:05 -0300

    a

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\samuel\OneDrive\Documentos\footlife\venv
+set VIRTUAL_ENV=C:\Users\samuel\OneDrive\Documentos\GitHub\footlife\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
